daa2bb75b3e71ed8a447aa190990f703e3360915
jaade <jaade@DESKTOP-4S5CDDG>
2019-10-27 20:54:23 +0100

    segon

--- src/UF2ACT2.java
+++ src/UF2ACT2.java
@@ -12,14 +12,14 @@ public class UF2ACT2 {
     Calendar calendario = new GregorianCalendar();
     System.out.println("Inici: "+ calendario.get(Calendar.HOUR_OF_DAY) + ":" + calendario.get(Calendar.MINUTE) +
         ":" + calendario.get(Calendar.SECOND));
-    // Crea un pool de 2 fils
-    final ScheduledExecutorService schExService = Executors.newScheduledThreadPool(2);
+    // Crea un pool de 4 fils
+    final ScheduledExecutorService schExService = Executors.newScheduledThreadPool(4);
     // Crea objecte Runnable.
     final Runnable ob = new UF2ACT2().new ExecutaFil();
-    // Programa Fil, s’inicia als 2 segons i després es va executant cada 3 segons
-    schExService.scheduleWithFixedDelay(ob, 2, 3, TimeUnit.SECONDS);
-    // Espera per acabar 10 segons
-    schExService.awaitTermination(10, TimeUnit.SECONDS);
+    // Programa Fil, s’inicia als 5 segons i després es va executant cada 6 segons
+    schExService.scheduleWithFixedDelay(ob, 5, 6, TimeUnit.SECONDS);
+    // Espera per acabar 6 segons
+    schExService.awaitTermination(6, TimeUnit.SECONDS);
     // shutdown .
     schExService.shutdownNow();
     System.out.println("Completat");
